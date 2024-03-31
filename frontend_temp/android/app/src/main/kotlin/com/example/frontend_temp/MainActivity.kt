c4b5d6606131b8572781d89574253ab371acd43e
piyushsethia07 <piyushsethia128@gmail.com>
2024-03-31 21:36:25 +0530

    stable v1

---
+++ frontend_temp/android/app/src/main/kotlin/com/example/frontend_temp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frontend_temp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ frontend_temp/lib/transaction/service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:frontend_temp/transaction/model.dart';
+
+class TransactionService {
+  Future<List<Transaction>> fetchTransactions() async {
+    final response = await http.get(Uri.parse(
+        'https://calculator-service-pk-embedded-solution.koyeb.app/api/transactions'));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      return jsonResponse.map((data) => Transaction.fromJson(data)).toList();
+    } else {
+      throw Exception('Failed to load transactions');
+    }
+  }
+
+  Future<void> addTransaction(Transaction transaction) async {
+    final response = await http.post(
+      Uri.parse(
+          'https://calculator-service-pk-embedded-solution.koyeb.app/api/transactions'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'mobile_number': transaction.mobileNumber,
+        'transaction_amount': transaction.transactionAmount,
+        'transaction_type': transaction.transactionType,
+        'cash_in_out': transaction.cashInOut,
+        'person_name': transaction.personName,
+      }),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to add transaction');
+    }
+  }
+}
